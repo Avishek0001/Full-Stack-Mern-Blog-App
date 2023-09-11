b9760cfdd8e771918156cf51d0158ba582373dbd
Avishek Kundu <92811606+Avishek0001@users.noreply.github.com>
2023-09-11 21:41:11 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
 
   async function handleSubmit(event) {
     event.preventDefault();
-    try{const response = await fetch("https://serverside-mern-blog-app.vercel.app/api/v1/user/register", {
+    try{const response = await fetch("https://serverside-mern-blog-llvozs0h1-avishek0001.vercel.app/api/v1/user/register", {
     // try{const response = await fetch("http://localhost:4000/api/v1/user/register", {
       method: "POST",
       body: JSON.stringify({
