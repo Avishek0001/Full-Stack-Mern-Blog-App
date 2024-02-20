52f459fda45d5bfd80e0749cd45a8503aca88905
Avishek Kundu <kunduavishek112233@gmail.com>
2024-02-20 22:12:34 +0530

    updated env

--- api/index.js
+++ api/index.js
@@ -8,10 +8,11 @@ const cookieParser = require("cookie-parser")
 const multer = require("multer")
 
 const BASE_URL = process.env.BASE_URL || 4000
+const database_url = process.env.DATABASE_URL
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
-mongoose.connect("mongodb+srv://Avishek-Mern-Blog:avishek2001@cluster0.uhuwjdg.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(database_url)
 
 const userRoute= require("./routes/userRoute")
 const blogsRoutes = require("./routes/blogsRoute")
